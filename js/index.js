1a01368c6b215f86315bff8cb26ea9fbb42d34f8
Balakleenko Mikhail <enot42@gmail.com>
2019-01-21 01:50:46 +0200

    first production

---
+++ js/index.js
@@ -0,0 +1,33 @@
+$('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  arrows: false,
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true
+});
+
+ 
+$(document).ready(function(){
+	$("#menu").on("click","a", function (event) {
+		//отменяем стандартную обработку нажатия по ссылке
+		event.preventDefault();
+
+		//забираем идентификатор бока с атрибута href
+		var id  = $(this).attr('href'),
+
+		//узнаем высоту от начала страницы до блока на который ссылается якорь
+			top = $(id).offset().top;
+		
+		//анимируем переход на расстояние - top за 1500 мс
+		$('body,html').animate({scrollTop: top-182}, 1500);
+	});
+});
